30d998084d044a916dd0cf18ed3377c455414588
Ted Collins <theodore.collins2023@outlook.com>
2024-05-20 15:38:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment3._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 2, 1, 1, 9, 1, 1 };
+            int target = 1;
+
+            Console.Write("INPUT: ");
+            PrintArray(numbers);
+            FindConsecutiveMatchingInts(numbers, target);
+            Console.Write("OUTPUT: ");
+            PrintArray(numbers);
+        }
+
+        public static void FindConsecutiveMatchingInts(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target && (nums[i +1]) == target)
+                {
+                    nums[i] = 0;
+                    nums[i + 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        public static void PrintArray(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
